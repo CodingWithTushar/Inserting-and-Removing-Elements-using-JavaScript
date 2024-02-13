97e93399a1bf57212287d1cddedca1cbc22c5c14
Tushar <tushard3998@gmail.com>
2024-02-13 15:09:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+console.log("It's Working")
+
+/*document.querySelector(".container").innerHTML
+
+document.getElementsByClassName("container")*/
+
+//HTML INNER AND OUTER
+
+/*document.querySelector(".container").innerHTML
+
+document.querySelector(".container").outerHTML*/
+
+//Text 
+
+/*document.querySelector(".container").textContent
+
+document.querySelector(".box").innerText
+
+document.querySelector(".box").outerText*/
+
+//HIDDEN
+
+/*document.querySelector(".container").hidden
+
+document.querySelector(".container").hidden = true*/
+
+//ATTRIBUTES RELATED
+
+//1.Has Attributes
+
+/*document.querySelector(".container").hasAttribute("style")
+false
+document.querySelector(".box").hasAttribute("style")
+true*/
+
+//2.Get Attributes
+
+//document.querySelector(".box").getAttribute("style")
+
+//3.Set Attributes
+
+//document.querySelector(".box").setAttribute("style" ," display:grid;")
+
+//4.Remove Attributes
+
+//document.querySelector(".box").removeAttribute("style")
+
+//5.Attributes
+
+//document.querySelector(".box").attributes
+
+/*Node Name
+
+document.querySelector(".container").nodeName*/
+
+/*Remove
+
+document.querySelector(".box").remove()*/
+
+//CLASSLISTS
+
+/*document.querySelector(".container").classList
+
+Class Name
+
+document.querySelector(".box").className
+
+Add New Class
+
+document.querySelector(".container").classList.add("Hlo")
+
+Remove Class
+
+document.querySelector(".container").classList.remove("Hlo")*/
+
+
+
